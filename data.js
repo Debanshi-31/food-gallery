fdc2fd9c3de5cf9cd336983ce238902d740ca237
Debanshi-31 <141849473+Debanshi-31@users.noreply.github.com>
2024-01-24 09:54:29 +0530

    newrepo

---
+++ data.js
@@ -0,0 +1,203 @@
+//filter categories
+
+const categories = [
+    {
+        name:'All',
+        filter:'all',
+    },
+    {
+        name:'Burger',
+        filter:'burger',
+    },
+    {
+        name:'Drinks',
+        filter:'drinks',
+    },
+    {
+        name:'Dessert',
+        filter:'dessert',
+    },
+    {
+        name:'Pasta',
+        filter:'pasta',
+    },
+];
+
+const images = [
+    {
+        id: '1',
+        url :'desert1.avif',
+        category: 'dessert',
+        title: 'dessert1',
+    },
+    {
+        id: '2',
+        url:'desert2.avif ',
+        category: 'dessert',
+        title: 'dessert2',
+    },
+    {
+        id: '3',
+        url:'desert3.avif ',
+        category: 'dessert',
+        title: 'dessert3',
+    },
+    {
+        id: '4',
+        url:'desert4.avif ',
+        category: 'dessert',
+        title: 'dessert4',
+    },
+
+    {
+        id: '5',
+        url:'desert5.avif ',
+        category: 'dessert',
+        title: 'dessert5',
+    },
+    {
+        id: '6',
+        url:'desert6.avif ',
+        category: 'dessert',
+        title: 'dessert6',
+    },
+    {
+        id: '7',
+        url:'desert7.avif ',
+        category: 'dessert',
+        title: 'dessert17',
+    },
+    {
+        id: '8',
+        url:'burger1.avif ',
+        category: 'burger',
+        title: 'burger1',
+    },
+    {
+        id: '9',
+        url:'burger2.avif ',
+        category: 'burger',
+        title: 'burger2',
+    },
+    {
+        id: '10',
+        url:'burger3.avif',
+        category: 'burger',
+        title: 'burger3',
+    },
+    {
+        id: '11',
+        url:'burger4.avif ',
+        category: 'burger',
+        title: 'burger4',
+    },
+    {
+        id: '12',
+        url:'burger5.avif ',
+        category: 'burger',
+        title: 'burger5',
+    },
+    {
+        id: '13',
+        url:'burger6.avif ',
+        category: 'burger',
+        title: 'burger6',
+    },
+    {
+        id: '14',
+        url:'drink1.avif ',
+        category: 'drinks',
+        title: 'drink1',
+    },
+    {
+        id: '15',
+        url:'drink2.avif ',
+        category: 'drinks',
+        title: 'drink2',
+    },
+    {
+        id: '16',
+        url:'drink3.avif ',
+        category: 'drinks',
+        title: 'drink3',
+    },
+    {
+        id: '17',
+        url:'drink4.avif ',
+        category: 'drinks',
+        title: 'drink4',
+    },
+    {
+        id: '18',
+        url:'drink5.avif ',
+        category: 'drinks',
+        title: 'drink5',
+    },
+    {
+        id: '19',
+        url:'drink6.avif ',
+        category: 'drinks',
+        title: 'drink6',
+    },
+    {
+        id: '20',
+        url:'drink7.avif ',
+        category: 'drinks',
+        title: 'drink7',
+    },
+    {
+        id: '21',
+        url:'pasta1.avif ',
+        category: 'pasta',
+        title: 'pasta1',
+    },
+
+    {
+        id: '22',
+        url:'pasta2.avif ',
+        category: 'pasta',
+        title: 'pasta2',
+    },
+    {
+        id: '23',
+        url:'pasta3.avif ',
+        category: 'pasta',
+        title: 'pasta3',
+    },
+    {
+        id: '24',
+        url:'pasta4.avif ',
+        category: 'pasta',
+        title: 'pasta4',
+    },
+    {
+        id: '25',
+        url:'pasta5.avif ',
+        category: 'pasta',
+        title: 'pasta5',
+    },
+    {
+        id: '26',
+        url:'pasta6.avif ',
+        category: 'pasta',
+        title: 'pasta6',
+    },
+    {
+        id: '27',
+        url:'pasta7.avif ',
+        category: 'pasta',
+        title: 'pasta7',
+    },
+    {
+        id: '28',
+        url:'pasta8.avif ',
+        category: 'pasta',
+        title: 'pasta8',
+    },
+    {
+        id: '29',
+        url:'pasta9.avif ',
+        category: 'pasta',
+        title: 'pasta9',
+    },
+];
